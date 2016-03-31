5c9430494129dfb51123de5fc5f1bddb804efaac
Vlados <vladpereskokov@mail.ru>
2016-03-31 09:25:37 +0300

    new stack.h

--- Lab metod 3/stack/stack.h
+++ Lab metod 3/stack/stack.h
@@ -1,84 +1,47 @@
 struct stack {
-	stack()
-	:size_(100), head_(0), value_(new int[100]) {
-        memset(value_, 0, size_);
+public:
+    stack()
+    :head_(nullptr) {}
+    
+    ~stack() {
+        node *temp = head_;
+        while(temp != nullptr) {
+            head_ = head_->pnext_;
+            delete temp;
+            temp = head_;
+        }
     }
-
-	stack(const int size)
-	:size_(size), head_(0), value_(new int[size]) {
-        memset(value_, 0, size_);
+    
+    void add(int &data) {
+        node *temp = new node(data);
+        temp->pnext_ = head_;
+        head_ = temp;
+        delete temp;
     }
-
-	stack(const stack &copy)
-	:size_(copy.size_), head_(copy.head_), value_(new int[copy.size_]) {
-		for (size_t i = 0; i < head_; ++i)
-			value_[i] = copy.value_[i];
-	}
-
-	~stack() {
-		delete [] value_;
-	}
-
-	bool add(const int value) {
-		if (head_ >= size_)
-			return false;
-		value_[head_++] = value;
-		return true;
-	}
-
-	bool del() {
-		if (head_ <= 0)
-			return false;
-		--head_;
-		return true;
-	}
-
-	int get() const {
-		if (head_ < 0)
-			return -1;
-		return value_[head_ - 1];
-	}
+    
+    int get() {
+        return head_->data_;
+    }
+    
+    bool del() {
+        if (isEmpty())
+            return false;
+        node *tmp = head_;
+        head_ = head_->pnext_;
+        delete tmp;
+        return true;
+    }
+    
+    bool isEmpty() {
+        return head_ ? false : true;
+    }
 private:
-	int *value_;
-	int head_;
-	int size_;
+    struct node {
+        node()
+        :pnext_(nullptr), data_(0) {}
+        
+        node *pnext_;
+        int data_;
+    };
+    node *head_;
 };
-
-//struct stack
-//{
-//    stack(tdata data, stack *next)
-//    : data_(data), next_(next) {}
-//    
-//    ~Stack() = default;
-//    
-//    int	data_;
-//    stack	*next_;
-//};
-//
-//
-//class StackController
-//{
-//public:
-//    StackController()
-//    : head(nullptr) {}
-//    ~StackController() = default;
-//    
-//    
-//    void	add(tdata data) {
-//        stack *newElement = new Stack(data, head);
-//        head = newElement;
-//    }
-//    
-//    void	del(void) {
-//        stack *deleted = head;
-//        head = head->next;
-//        
-//        delete deleted;
-//    }
-//    
-//    int	get(void) {
-//        return head->data;
-//    }
-//private:
-//    stack	*head;
-//};

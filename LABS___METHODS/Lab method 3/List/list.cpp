9dfcb36fb04061cc57189a90680a3ccd08d26805
vladpereskokov <vladpereskokov@mail.ru>
2016-05-27 19:56:37 +0300

    Update list.cpp

--- LABS___METHODS/Lab method 3/List/list.cpp
+++ LABS___METHODS/Lab method 3/List/list.cpp
@@ -1,58 +1,44 @@
 #include "list.h"
 
 list::list()
-:head_(nullptr)
+:head_(nullptr), cursor_(nullptr)
 {}
 
-list::list(const list &copy)
-:head_(copy.head_)
-{}
-
-void list::add(const std::size_t index, const int data) {
-    list::node *new_node = new node(data);
-    if (head_ == nullptr)
-        head_ = new_node;
-    else {
-        list::node *temp = head_;
-        std::size_t i = 0;
-        while (i++ < index) {
-            temp->_pnext = new_node;
-            temp = temp->_pnext;
-        }
+void list::add(int index, const int data) {
+    cursor_ = head_;
+    node* newElement = new node(data);
+    --index;
+    if (index < 0) {
+        newElement->_pnext = head_;
+        head_ = newElement;
+        return;
     }
+    while (--index >= 0)
+        cursor_ = cursor_->_pnext;
+    newElement->_pnext = cursor_->_pnext;
+    cursor_->_pnext = newElement;
 }
 
-void list::del(const std::size_t index) {
-    list::node *temp = head_;
-    if (index == 0) {
+void list::del(int index) {
+    cursor_ = head_;
+    --index;
+    if (index < 0) {
         head_ = head_->_pnext;
-        delete temp;
+        delete cursor_;
+        return;
     }
-    else if (index > 0) {
-        list::node *new_node = head_;
-        for (std::size_t i = 1; i < index - 1; ++i)
-            new_node = new_node->_pnext;
-        temp = new_node->_pnext;
-        new_node->_pnext = new_node->_pnext->_pnext;
-        delete temp;
-    }
+    while (--index >= 0)
+        cursor_ = cursor_->_pnext;
+    node *deleted = cursor_->_pnext;
+    cursor_->_pnext = cursor_->_pnext->_pnext;
+    delete deleted;
 }
 
-int list::get(const std::size_t index) const {
-    list::node *temp = head_;
-    std::size_t i = 0;
-    while (i++ < index)
-        temp = temp->_pnext;
-    return temp->_data;
+int list::get(int index) {
+    cursor_ = head_;
+    while (--index >= 0)
+        cursor_ = cursor_->_pnext;
+    return cursor_->_data;
 }
 
-list::~list() {
-    list::node *temp = head_;
-    list::node *delete_node = head_;
-    while (delete_node != nullptr) {
-        temp = temp->_pnext;
-        delete delete_node;
-        delete_node = temp;
-    }
-    head_ = nullptr;
-}
+list::~list() {}
